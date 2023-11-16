4aa74909ae8a36626ead0b9820ae94dd18c01927
onkar <jadhavonkar333@gmail.com>
2023-11-16 16:04:05 +0530

    Assignment14

---
+++ Assignment14/LargeDataCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment14
+{
+    public class LargeDataCollection : IDisposable
+    {
+        private List<object> data;
+
+        public LargeDataCollection(IEnumerable<object> initialdata)
+        {
+            data = new List<object>(initialdata);
+        }
+        public void AddElement(object element)
+        {
+            data.Add(element);
+        }
+        public void RemoveElement(object element)
+        {
+            data.Remove(element);
+        }
+        public object GetElement(int index)
+        {
+            if (index >= 0 && index < data.Count)
+            {
+                return data[index];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("index out of ranges");
+            }
+        }
+        public void Dispose()
+        {
+            data.Clear();
+            data = null;
+        }
+    }
+}
---
+++ Assignment14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> initialdata = new List<object>() { 1, 2, 3, "four", "five" };
+            using (LargeDataCollection largeData = new LargeDataCollection(initialdata))
+            {
+                largeData.AddElement(6);
+                largeData.AddElement(7);
+
+                Console.WriteLine("Elements in LargedataCollection");
+                for (int i = 0; i < initialdata.Count; i++)
+                {
+                    Console.WriteLine(largeData.GetElement(i));
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
